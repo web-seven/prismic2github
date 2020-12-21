df7025fb3ed9f6129ca6f4200879f00f82467a6b
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2020-12-21 12:48:52 +0200

    proxy application and docker image builder

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node
+
+WORKDIR /usr/src/app
+
+COPY ./ /usr/src/app
+
+RUN "npm install"
+
+EXPOSE 8080
+
+CMD [ "node", "src/index.js" ]
--- src/index.js
+++ src/index.js
@@ -1,27 +1,55 @@
 const http = require('http');
-const fs = require('fs');
+const { Octokit } = require("@octokit/rest");
 
 const hostname = process.env.PROXY_HOST_NAME ? process.env.PROXY_HOST_NAME : '0.0.0.0';
 const port = process.env.PROXY_PORT ? process.env.PROXY_PORT : 8080;
 
 const server = http.createServer((req, res) => {
     let payload = '';
-    req.on('data', (chunk) =>{
+    req.on('data', (chunk) => {
         payload += chunk;
     });
 
-    req.on('end', (chunk) =>{
-        if(payload.length > 0) {
+    req.on('end', () => {
+        if (payload.length > 0) {
             const data = JSON.parse(payload);
-            console.debug(data);
-            // const token = data.secret;
-            // const [basepath, owner, repo, eventType] = req.url.replace(/^\/|\/$/g, '').split('/');
-            // console.debug(basepath, owner, repo, eventType);
-            res.statusCode = 200;
-            res.end('Event Triggered.');
+            const [basepath, owner, repo, eventType] = req.url.replace(/^\/|\/$/g, '').split('/');
+            if (!owner || !repo || !eventType) {
+                res.end('Github repository owner, repo name or event type not found, please define it in URL eg.: /prismic/{owner}/{repo}/{event_type}');
+                return;
+            }
+            if (!data.secret) {
+                res.end('Secret key not found in prismic webhook payload, please define setup it as value of Github Access Token.');
+                return;
+            }
+            const token = data.secret;
+            const octokit = new Octokit({
+                auth: token,
+            });
+
+            octokit.repos
+                .createDispatchEvent({
+                    owner: owner,
+                    repo: repo,
+                    event_type: eventType,
+                    client_payload: data
+                })
+                .then(({ status }) => {
+                    if (status === 204) {
+                        res.statusCode = 200;
+                        res.end(`Event "${eventType}" was successfully triggered on "${owner}/${repo}".`);
+                    } else {
+                        res.statusCode = status;
+                        res.end('Event Not Triggered.');
+                    }
+                }).catch((e) => {
+                    console.debug(e);
+                    res.end('Event Not Triggered: ' + e.message);
+                });
+
         } else {
             res.statusCode = 500;
-            res.end('Prismic webhook payload not found in rquest.');
+            res.end('Prismic webhook payload not found in request.');
         }
     });
 });
