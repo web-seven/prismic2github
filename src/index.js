6ff25e18a1cc95c2f7d2fa590de64cee6db9e812
Evgheni Poleacov <evgheni.poleacov@gmail.com>
2020-12-21 22:52:09 +0200

    switched proxy to workflow dispatch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const semver = require('semver');
+const url = require('url');
 const { Octokit } = require("@octokit/rest");
 
 const hostname = process.env.PROXY_HOST_NAME ? process.env.PROXY_HOST_NAME : '0.0.0.0';
@@ -11,13 +11,16 @@ const server = http.createServer((req, res) => {
         payload += chunk;
     });
 
-    req.on('end', async () => {
+    req.on('end', () => {
         if (payload.length > 0) {
             const data = JSON.parse(payload);
-            const [basepath, owner, repo, branch] = req.url.replace(/^\/|\/$/g, '').split('/');
-            if (!owner || !repo || !branch) {
+            const urlObject = url.parse(req.url,true);
+            const queryParams = urlObject.query;
+            const [basepath, owner, repo] = urlObject.pathname.replace(/^\/|\/$/g, '').split('/');
+            if (!owner || !repo || !queryParams.branch || !queryParams.workflow) {
                 res.statusCode = 400;
-                res.end('Github repository owner, repo name or branch not found, please define it in URL eg.: /prismic/{owner}/{repo}/{branch}');
+                res.end('Github repository owner, repo name or branch not found, please define it in URL eg.:'
+                        +' /prismic/{owner}/{repo}?branch={branch_name}&workflow={workflow_id}');
                 return;
             }
             if (!data.secret) {
@@ -33,66 +36,33 @@ const server = http.createServer((req, res) => {
             let releaseName = ''
 
             if (data.releases.addition) {
-                releaseName = data.releases.addition[0].label;
+                releaseName = data.releases.addition[0].id;
             } else if (data.releases.update) {
-                releaseName = data.releases.update[0].label;
-            } else if (data.releases.deletion) {
-                releaseName = data.releases.deletion[0].label;
+                releaseName = data.releases.update[0].id;
             } else {
                 res.statusCode = 400;
                 res.end('Payload not contain data for create or update release');
                 return;
             }
 
-            let version = semver.valid(semver.coerce(releaseName));
-            if (!version) {
-                res.statusCode = 400;
-                res.end('Just SemVer lables with wildcards supported.');
-                return;
-            }
-
-            let tagsCount = 100;
-            let page = 0;
-
-            while (tagsCount >= 100) {
-                page++;
-                let tags = await octokit.repos.listTags({
-                    owner: owner,
-                    repo: repo,
-                    per_page: 100,
-                    page: page,
-                });
-                tags.data.forEach(tag => {
-                    if (semver.valid(tag.name) && semver.satisfies(tag.name, releaseName)) {
-                        if (semver.gt(tag.name, version) || semver.eq(tag.name, version)) {
-                            version = semver.inc(semver.valid(tag.name), 'patch');
-                        }
-                    }
-                })
-                tagsCount = tags.data.length;
-            }
-
-            octokit.repos.getBranch({
+            octokit.actions.createWorkflowDispatch({
                 owner: owner,
                 repo: repo,
-                branch: branch
-            }).then((branchData) => {
-                octokit.repos.createRelease({
-                    owner: owner,
-                    repo: repo,
-                    name: "Prismic.io release: " + releaseName,
-                    tag_name: version,
-                    target_commitish: branchData.data.commit.sha
-                }).then((releaseData) => {
-                    if (releaseData.status === 201) {
-                        res.end(`Release ${version} linked to ${branchData.data.commit.sha} was successfully creates in "${owner}/${repo}".`);
-                    } else {
-                        res.end('Release not created, don\'t know reason');
-                    }
-                }).catch(e => {
-                    res.end('Release not created, reason: ' + e.message);
-                })
-            });
+                ref: queryParams.branch,
+                workflow_id: queryParams.workflow,
+                inputs: {
+                    release: releaseName
+                }
+            }).then((releaseData) => {
+                if (releaseData.status === 204) {
+                    res.end(`Release "${releaseName}" was successfully sent to "${owner}/${repo}/${queryParams.branch}/${queryParams.workflow}".`);
+                } else {
+                    res.end('Release did not sent, don\'t know reason');
+                }
+            }).catch(e => {
+                res.end('Release did not created, reason: ' + e.message);
+            })
+       
         } else {
             res.statusCode = 400;
             res.end('Prismic webhook payload not found in request.');
